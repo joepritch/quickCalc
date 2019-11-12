8ff9b0c4f908fd5c4a0830a972e84ed39bc79587
Joe Pritchett <joempritchett@gmail.com>
2019-11-12 06:27:48 -0800

    add temporary numberpad layout

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-export default function CalcButton() {
+export default function CalcButton(props) {
   return (
     <View style={styles.container}>
-        <Text>Button</Text>
+        <Text>{props.value}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 80,
-    width: '20%',
+    flex: 1,
     backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
+    flexBasis: '20%',
+    margin: '5%',
   },
 });
--- src/components/Numberpad.js
+++ src/components/Numberpad.js
@@ -5,7 +5,21 @@ import CalcButton from './CalcButton.js';
 export default function NumberPad() {
   return (
     <View style={styles.container}>
-        <CalcButton/>
+        <CalcButton value='1'/>
+        <CalcButton value='2'/>
+        <CalcButton value='3'/>
+        <CalcButton value='4'/>
+        <CalcButton value='5'/>
+        <CalcButton value='6'/>
+        <CalcButton value='7'/>
+        <CalcButton value='8'/>
+        <CalcButton value='9'/>
+        <CalcButton value='DEL'/>
+        <CalcButton value='+'/>
+        <CalcButton value='-'/>
+        <CalcButton value='*'/>
+        <CalcButton value='/'/>
+        <CalcButton value='='/>
     </View>
   );
 }
@@ -15,7 +29,8 @@ const styles = StyleSheet.create({
     height: '75%',
     width: '100%',
     backgroundColor: 'grey',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignContent: 'stretch',
   },
 });
